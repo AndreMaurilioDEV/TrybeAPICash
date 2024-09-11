071c2f1c5bc9d47cfa924e28c9fdc5bb26e81c0d
André Maurilio <117940329+AndreMaurilioDEV@users.noreply.github.com>
2024-09-11 15:18:47 -0300

    Update connection.js

--- src/db/connection.js
+++ src/db/connection.js
@@ -6,8 +6,8 @@ const mysql = require('mysql2/promise');
 const connection = mysql.createPool({
   host: process.env.MYSQL_HOST || 'localhost',
   port: process.env.MYSQL_PORT || 33060,
-  user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'root',
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
   database: process.env.DB_NAME || 'trybecashdb',
   waitForConnections: true,
   connectionLimit: 10,
